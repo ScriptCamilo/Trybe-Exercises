bc98217a3385a87e17f64f900bb6a5b5f8717c79
ScriptCamilo <rodrigo.camilo228@gmail.com>
2021-02-25 15:08:03 -0300

    Bloco 4.2: Segundo exercicio feito

--- exercises/bloco_4/dia_2/scripts.js
+++ exercises/bloco_4/dia_2/scripts.js
@@ -4,3 +4,10 @@ let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 for (i = 0; i < numbers.length; i++) {
   console.log(numbers[i]);
 }
+
+// 2. Para o segundo exercício, você deve somar todos os valores contidos no array e imprimir o resultado
+let soma = 0;
+for (i of numbers) {
+  soma += i;
+}
+console.log(`A soma dos valores é: ${soma}`)
