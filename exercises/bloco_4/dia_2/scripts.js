4b52382ca95c465e337a06f455883165ea6d9133
ScriptCamilo <rodrigo.camilo228@gmail.com>
2021-02-25 16:42:57 -0300

    Bloco 4.2: Refatoraçao simples

--- exercises/bloco_4/dia_2/scripts.js
+++ exercises/bloco_4/dia_2/scripts.js
@@ -4,6 +4,7 @@ let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 for (i = 0; i < numbers.length; i++) {
   console.log(numbers[i]);
 }
+console.log('')
 
 // 2. Para o segundo exercício, você deve somar todos os valores contidos no array e imprimir o resultado
 let soma = 0;
@@ -11,11 +12,13 @@ for (i of numbers) {
   soma += i;
 }
 console.log(`A soma dos valores é: ${soma}`)
+console.log('')
 
 // 3. Para o terceiro exercício, calcule e imprima a média aritmética dos valores contidos no array
 let media;
 media = soma/numbers.length
 console.log(`A média aritmética é: ${media}`)
+console.log('')
 
 /*
   4. Com o mesmo código do exercício anterior, caso valor final seja maior que 20, imprima a mensagem: 
@@ -26,6 +29,7 @@ if (media > 20) {
 } else {
   console.log("Valor menor ou igual a 20")
 }
+console.log('')
 
 // 5. Utilizando for , descubra qual o maior valor contido no array e imprima-o;
 let maiorNumero = 0;
@@ -35,6 +39,7 @@ for (i of numbers) {
   }
 }
 console.log(`O maior número é: ${maiorNumero}`);
+console.log('')
 
 /*
   6. Descubra quantos valores ímpares existem no array e imprima o resultado. 
@@ -48,10 +53,11 @@ for (i of numbers) {
 }
 
 if (imparesCount > 0) {
-  console.log(`A quantidade de números ímpar é: ${imparesCount}`)
+  console.log(`A quantidade de números ímpares é: ${imparesCount}`)
 } else {
   console.log("Nenhum valor ímpar foi encontrado")
 }
+console.log('')
 
 // 7. Utilizando for , descubra qual o menor valor contido no array e imprima-o
 let menorNumero = 0;
@@ -61,10 +67,12 @@ for (i in numbers) {
   }
 }
 console.log(`O menor número é: ${numbers[menorNumero]}`)
+console.log('')
 
 // 8. Utilizando for , crie uma array que vá de 1 até 25 e imprima o resultado
 // 9. Utilizando o array criado no exercício anterior imprima o resultado da divisão de cada um dos elementos por 2
 for (i = 1; i <= 25; i++) {
   console.log(`Número: ${i}`)
-  console.log(`Divisão: ${i/2}`) // 9
+  console.log(`Divisão por 2: ${i/2}`) // 9
+  console.log('')
 }
