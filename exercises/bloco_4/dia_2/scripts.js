12af0f9c1099315fb1b35d179475b18609b35eab
ScriptCamilo <rodrigo.camilo228@gmail.com>
2021-02-25 18:11:03 -0300

    Bloco 4.2: Refatoraçao incrementaçao melhorada

--- exercises/bloco_4/dia_2/scripts.js
+++ exercises/bloco_4/dia_2/scripts.js
@@ -1,15 +1,15 @@
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 
 // 1. Nesse primeiro exercício, percorra o array imprimindo todos os valores nele contidos com a função console.log()
-for (index = 0; index < numbers.length; index++) {
+for (index = 0; index < numbers.length; index+=1) {
   console.log(numbers[index]);
 }
 console.log('')
 
 // 2. Para o segundo exercício, você deve somar todos os valores contidos no array e imprimir o resultado
 let soma = 0;
-for (number of numbers) {
-  soma += number;
+for (index of numbers) {
+  soma += index;
 }
 console.log(`A soma dos valores é: ${soma}`)
 console.log('')
