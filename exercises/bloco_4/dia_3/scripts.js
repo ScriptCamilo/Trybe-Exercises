25ef0345258e5dc01196d868982f450c770ec4ce
ScriptCamilo <rodrigo.camilo228@gmail.com>
2021-02-26 20:54:46 -0300

    Bloco 4.3: Terceiro exercicio feito

--- exercises/bloco_4/dia_3/scripts.js
+++ exercises/bloco_4/dia_3/scripts.js
@@ -1,15 +1,20 @@
 const symbol = '*'
-let n = 5;
+let number = 5;
 
 /*
  1. Para o primeiro exercício de hoje, faça um programa que, dado um valor n qualquer, seja n > 1 , 
  imprima na tela um quadrado feito de asteriscos de lado de tamanho n . 
 */
-for (index = 1; index <= n; index+=1) {
-  console.log(`${symbol.repeat(n)}`)
+for (index = 1; index <= number; index+=1) {
+  console.log(symbol.repeat(number))
 }
 
 // 2. Para o segundo exercício, faça o mesmo que antes, mas que imprima um triângulo retângulo com 5 asteriscos de base.
-for (index = 1; index <= n; index+=1) {
-  console.log(`${symbol.repeat(index)}`)
+for (index = 1; index <= number; index+=1) {
+  console.log(symbol.repeat(index))
+}
+
+// 3. Agora inverta o lado do triângulo.
+for (index = 1; index <= number; index+=1) {
+  console.log(' '.repeat(number-index) + symbol.repeat(index))
 }
