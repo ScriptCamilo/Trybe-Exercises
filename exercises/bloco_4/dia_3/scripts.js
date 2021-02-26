7d95e5e53ffe8b102453a6539f8b709aec95d358
ScriptCamilo <rodrigo.camilo228@gmail.com>
2021-02-26 20:32:57 -0300

    Bloco 4.3: Segundo exercicio feito

--- exercises/bloco_4/dia_3/scripts.js
+++ exercises/bloco_4/dia_3/scripts.js
@@ -1,11 +1,15 @@
 const symbol = '*'
-let n = 10;
+let n = 5;
 
 /*
  1. Para o primeiro exercício de hoje, faça um programa que, dado um valor n qualquer, seja n > 1 , 
  imprima na tela um quadrado feito de asteriscos de lado de tamanho n . 
 */
-
 for (index = 1; index <= n; index+=1) {
   console.log(`${symbol.repeat(n)}`)
+}
+
+// 2. Para o segundo exercício, faça o mesmo que antes, mas que imprima um triângulo retângulo com 5 asteriscos de base.
+for (index = 1; index <= n; index+=1) {
+  console.log(`${symbol.repeat(index)}`)
 }
