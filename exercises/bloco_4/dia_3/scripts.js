930ad78d0225df7f2964000aaca187add1b24330
ScriptCamilo <rodrigo.camilo228@gmail.com>
2021-02-27 00:04:13 -0300

    Bloco 4.3: Bonus inicializado

--- exercises/bloco_4/dia_3/scripts.js
+++ exercises/bloco_4/dia_3/scripts.js
@@ -24,3 +24,15 @@ for (index = 1; index <= count; index+=2) {
   let space = Math.floor((count-index)/2);
   console.log(' '.repeat(space) + symbol.repeat(index) + ' '.repeat(space))
 }
+
+// BÔNUS
+// 5. Faça uma pirâmide com n asteriscos de base que seja vazia no meio. Assuma que o valor de n será sempre ímpar.
+count = 7
+for (index = 1; index <= count; index+=2) {
+  let space = Math.floor((count-index)/2);
+  if (index === count) {
+    console.log(symbol.repeat(count))
+  } else {
+    console.log(' '.repeat(space) + symbol.repeat(1) + symbol.repeat(1) + ' '.repeat(space))
+  }
+}
