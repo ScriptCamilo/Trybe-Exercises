7f51d76bdeea03eb0996cf93ef58be657af7ae1d
ScriptCamilo <rodrigo.camilo228@gmail.com>
2021-02-28 17:03:14 -0300

    Block 4.1: First exercise done

---
+++ exercises/bloco_4/dia_1/ex01/scripts.js
@@ -0,0 +1,25 @@
+/*
+  1. Faça cinco programas, um para cada operação aritmética básica. Seu programa deve ter duas variáveis, a e b , 
+  definidas no começo com os valores que serão operados. Faça programas para:
+
+  Adição
+  Subtração
+  Multiplicação
+  Divisão
+  Módulo
+*/
+
+let firstNumber = 8;
+let secondNumber = 5;
+
+let sum = firstNumber + secondNumber;
+let difference = firstNumber - secondNumber;
+let product = firstNumber * secondNumber;
+let quotient = firstNumber / secondNumber;
+let modulo = firstNumber % secondNumber;
+
+console.log(`Sum: ${sum}`);
+console.log(`Difference: ${difference}`);
+console.log(`Product: ${product}`);
+console.log(`Quotient: ${quotient}`);
+console.log(`Modulo: ${modulo}`);
