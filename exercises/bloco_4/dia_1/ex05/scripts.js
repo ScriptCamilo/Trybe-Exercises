77594fbd2e9d074dbd6daa25abff7afc6840255d
ScriptCamilo <rodrigo.camilo228@gmail.com>
2021-02-28 20:13:48 -0300

    Block 4.1: Fifth exercise done

---
+++ exercises/bloco_4/dia_1/ex05/scripts.js
@@ -0,0 +1,17 @@
+/* 
+  Faça um programa que defina três variáveis com os valores dos três ângulos internos de um triângulo. 
+  Retorne true se os ângulos representarem os ângulos de um triângulo e false , caso contrário. 
+  Se algum ângulo for inválido o programa deve retornar uma mensagem de erro.
+*/
+
+let firstAngle = 90;
+let secondAngle = 45;
+let thirdAngle = -45;
+
+if (firstAngle < 0 || secondAngle < 0 || thirdAngle < 0) {
+  console.log('Error! Os ângulos de um triângulo não podem ser negativos.');
+} else if (firstAngle + secondAngle + thirdAngle === 180) {
+  console.log(true);
+} else {
+  console.log(false);
+}
