521a7b9d99fa8e23eb877dce3db7cacd410e030e
ScriptCamilo <rodrigo.camilo228@gmail.com>
2021-03-01 19:53:19 -0300

    Bloco 4.4: Part II second exercise done

--- exercises/bloco_4/dia_4/part_2/scripts.js
+++ exercises/bloco_4/dia_4/part_2/scripts.js
@@ -11,5 +11,25 @@ function isPalindrome(words) {
 }
 
 console.log(isPalindrome('Arara'));
+console.log();
 
 // 2.
+function greaterNumber(array) {
+  let greater = null;
+
+  for (let index in array) {
+    if (greater === null) {
+      greater = index;
+    } else if (greater < array[index]) {
+      greater = index;
+    } else {
+      continue;
+    }
+  }
+  return greater;
+}
+
+console.log(greaterNumber([2, 3, 6, 7, 10, 1]));
+console.log();
+
+// 3.
