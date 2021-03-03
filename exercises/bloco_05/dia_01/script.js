a8e90079871c13ab58eff3fcae0a91a84b7aa3e4
ScriptCamilo <rodrigo.camilo228@gmail.com>
2021-03-03 16:41:45 -0300

    Bloco 5.1: CSS code base

---
+++ exercises/bloco_05/dia_01/script.js
@@ -0,0 +1,7 @@
+let paragraphs = document.getElementsByClassName('paragraph');
+
+paragraphs[0].style.color = 'red';
+
+for (let index = 0; index < paragraphs.length; index += 1) {
+  paragraphs[index].style.fontSize = '34px';
+}
