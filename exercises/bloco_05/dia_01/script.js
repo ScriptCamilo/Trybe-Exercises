41d727bba5c4fa95068ed6cf5db7a0fa7176700d
ScriptCamilo <rodrigo.camilo228@gmail.com>
2021-03-04 08:57:19 -0300

    Bloco 5.1: Practice exercise done

--- exercises/bloco_05/dia_01/script.js
+++ exercises/bloco_05/dia_01/script.js
@@ -1,7 +1,14 @@
-let paragraphs = document.getElementsByClassName('paragraph');
-
-paragraphs[0].style.color = 'red';
-
-for (let index = 0; index < paragraphs.length; index += 1) {
-  paragraphs[index].style.fontSize = '34px';
+function changeBackgroundColor(element, color) {
+  let selector = document.querySelectorAll(element);
+  for (let index = 0; index < selector.length; index += 1){
+    selector[index].style.backgroundColor = color;
+  }
 }
+
+changeBackgroundColor('body', 'rgb(243, 243, 243)');
+changeBackgroundColor('header', 'rgb(0, 176, 105)');
+changeBackgroundColor('footer', 'rgb(0, 53, 51)');
+changeBackgroundColor('.emergency-tasks', 'rgb(255, 159, 132)');
+changeBackgroundColor('.emergency-tasks h3', 'rgb(165, 0, 243)');
+changeBackgroundColor('.no-emergency-tasks', 'rgb(249, 219, 94)');
+changeBackgroundColor('.no-emergency-tasks h3', 'rgb(35, 37, 37)');
