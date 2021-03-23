5ac0ef14ee95c62330b4a2d4827f303b0e8acfa5
ScriptCamilo <rodrigo.camilo228@gmail.com>
2021-03-23 20:14:04 -0300

    Segunda parte do exercicio com os tres primeiros pontos concluidos

---
+++ exercises/bloco_07/dia_01/part_II/script.js
@@ -0,0 +1,29 @@
+// First exercise
+const fatorial = (n) => (n <= 1) ? 1 : n * fatorial(n - 1);
+
+// console.log(fatorial(5));
+
+// Second exercise
+const longestWord = (setence) => {
+  const words = setence.split(' ');
+  let mainWord = '';
+  
+  for (let index = 0; index < words.length; index += 1) {
+    if (mainWord.length < words[index].length) {
+      mainWord = words[index];
+    }
+  }
+  return mainWord;
+};
+
+// console.log(longestWord('Antônio foi no banheiro e não sabemos o que aconteceu'));
+
+// Third exercise
+const clickCount = () => {
+  const count = Number(document.querySelector('#numberCount').textContent);
+
+  document.querySelector('#numberCount').innerText = count + 1 
+};
+
+const clickButton = document.querySelector('button');
+clickButton.addEventListener('click', clickCount);
