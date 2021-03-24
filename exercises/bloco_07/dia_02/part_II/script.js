b4abd778218ad12701d3c7f7e5563531b5c4eb17
ScriptCamilo <rodrigo.camilo228@gmail.com>
2021-03-24 18:01:51 -0300

    Bloco 7.2: Segunda parte inicializada, dois primeiros requisitos prontos

---
+++ exercises/bloco_07/dia_02/part_II/script.js
@@ -0,0 +1,26 @@
+const lesson1 = {
+  materia: 'Matemática',
+  numeroEstudantes: 20,
+  professor: 'Maria Clara',
+  turno: 'manhã',
+};
+
+const lesson2 = {
+  materia: 'História',
+  numeroEstudantes: 20,
+  professor: 'Carlos',
+};
+
+const lesson3 = {
+  materia: 'Matemática',
+  numeroEstudantes: 10,
+  professor: 'Maria Clara',
+  turno: 'noite',
+};
+
+const addEntry = (lesson, newKey, value) => {
+  lesson[newKey] = value;
+}
+addEntry(lesson2, 'turno', 'tarde');
+
+const listKeys = lesson => Object.keys(lesson);
