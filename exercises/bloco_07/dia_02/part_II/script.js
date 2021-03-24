16c03d3090f04bfc5bcd45d02da2d3833ff26d20
ScriptCamilo <rodrigo.camilo228@gmail.com>
2021-03-24 18:06:35 -0300

    Bloco 7.2: Quarto requisito feito

--- exercises/bloco_07/dia_02/part_II/script.js
+++ exercises/bloco_07/dia_02/part_II/script.js
@@ -18,11 +18,17 @@ const lesson3 = {
   turno: 'noite',
 };
 
+// 1
 const addEntry = (lesson, newKey, value) => {
   lesson[newKey] = value;
 }
 addEntry(lesson2, 'turno', 'tarde');
 
+// 2
 const listKeys = lesson => Object.keys(lesson);
 
+// 3
 const objLenght = lesson => Object.keys(lesson).length;
+
+// 4
+const listValues = lesson => Object.values(lesson);
