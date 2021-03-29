35f284df7b915cd59e5cce4578a0d89c6c8e2cca
ScriptCamilo <rodrigo.camilo228@gmail.com>
2021-03-29 19:14:30 -0300

    Bloco 8.1: exercicio bonus done

---
+++ exercises/bloco_08/bonus/game.js
@@ -0,0 +1,64 @@
+const mage = {
+  healthPoints: 130,
+  intelligence: 45,
+  mana: 125,
+  damage: undefined,
+};
+
+const warrior = {
+  healthPoints: 200,
+  strength: 30,
+  weaponDmg: 2,
+  damage: undefined,
+};
+
+const dragon = {
+  healthPoints: 350,
+  strength: 50,
+  damage: undefined,
+};
+
+const battleMembers = { mage, warrior, dragon };
+
+const dragonDamage = () => Math.floor(Math.random() * (dragon.strength - 15 + 1) + 15);
+
+const warriorDamage = () => { 
+  return Math.floor(Math.random() * (warrior.strength * warrior.weaponDmg - warrior.strength + 1) + warrior.strength);
+}
+
+const mageDamage = () => {
+  const damage = Math.floor(Math.random() * (mage.intelligence * 2 - mage.intelligence + 1) + mage.intelligence);
+  const mana = (mage.mana > 15) ? mage.mana - 15 : mage.mana;
+
+  if (mana === mage.mana) {
+    return 'Não possui mana suficiente';
+  }
+  return { damage, mana }
+}
+
+
+const gameActions = {
+  warriorAction: (action) => {
+    warrior.damage = action();
+    dragon.healthPoints -= warrior.damage;
+  },
+  mageAction: (action) => {
+    const { damage, mana } = action();
+    mage.damage = damage;
+    mage.mana = mana;
+    dragon.healthPoints -= damage;
+  },
+  dragonAction: (action) => {
+    dragon.damage = action();
+    warrior.healthPoints -= dragon.damage;
+    mage.healthPoints -= dragon.damage;
+  },
+  update: () => {
+    return battleMembers;
+  }
+};
+
+gameActions.warriorAction(warriorDamage);
+gameActions.mageAction(mageDamage);
+gameActions.dragonAction(dragonDamage);
+console.log(gameActions.update());
